b33742b0641c732bdaaeade99c23bb5743eeb3c1
ChrisAchinga <achinga.chris@gmail.com>
2020-10-01 12:50:34 +0300

    connect/disconnect

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
