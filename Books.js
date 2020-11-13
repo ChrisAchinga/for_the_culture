13f51d38bf305fc5e26ed7d0e7eba59dd80403e2
ChrisAchinga <achinga.chris@gmail.com>
2020-11-13 19:22:58 +0300

    books

---
+++ Books.js
@@ -0,0 +1,28 @@
+const books = [
+  {
+    id: 1,
+    title: 'JavaScript for Dummies',
+    author: 'Kenya Baris',
+    publisher: 'Quality Control',
+    date: 'May-20-2021',
+    website: 'www.readbooks.com/javascript-for-dummies',
+  },
+  {
+    id: 2,
+    title: 'Node for Dummies',
+    author: 'Kenya Baris',
+    publisher: 'Quality Control',
+    date: 'May-20-2021',
+    website: 'www.readbooks.com/javascript-for-dummies',
+  },
+  {
+    id: 3,
+    title: 'CSS for Dummies',
+    author: 'Kenya Baris',
+    publisher: 'Quality Control',
+    date: 'May-20-2021',
+    website: 'www.readbooks.com/javascript-for-dummies',
+  }
+]
+
+module.exports = books
